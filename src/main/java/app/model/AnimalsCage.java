9a59de8bdc4bc117d726c0df3874981e29057722
frutodelaranja <frutodelaranja@gmail.com>
2020-05-12 00:11:16 +0300

    add_project

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,22 @@
+package app.model;
+
+import app.model.Animal;
+import app.model.Timer;
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    private Animal animal;
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
+        System.out.println("________________________");
+    }
+}
